6f9506e21a6b818ec51850f2085680aaa745d37f
gitproject23 <n.sreenu23@gmail.com>
2023-07-30 15:51:06 +0530

    first commit

---
+++ src/com/soft/Test.java
@@ -0,0 +1,14 @@
+package com.soft;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i=0;i<20;i++){
+            if(i%2==0){
+                System.out.println("even number"+i);
+            }else{
+                System.out.println("add number"+i);
+            }
+
+        }
+    }
+}
